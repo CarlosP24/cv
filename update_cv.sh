6ef89c265868b6bf51773c2ed6c51108ff9d0a72
Carlos Payá <carlos.paya@csic.es>
2023-12-30 22:44:28 +0100

    sh maker updated

--- update_cv.sh
+++ update_cv.sh
@@ -10,3 +10,5 @@ python3 libraries/tsv_to_bib_funding.py database/funding.tsv database/funding.bi
 python3 libraries/tsv_to_bib_awards.py database/awards.tsv database/awards.bib
 
 latexmk -synctex=1 -interaction=nonstopmode -file-line-error -lualatex CV.tex
+
+cp CV.pdf ../Personal_web/files/
