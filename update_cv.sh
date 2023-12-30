1be7c883993f11cbe0041a33eb184889eb0b0e07
Carlos Payá <carlos.paya@csic.es>
2023-12-30 22:43:31 +0100

    sh updater

---
+++ update_cv.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Update CV
+
+# Update bib databases
+# Update publications in web first!! 
+
+python3 libraries/tsv_to_bib.py ../Personal_web/markdown_generator/outreach.tsv database/outreach.bib
+python3 libraries/tsv_to_bib.py ../Personal_web/markdown_generator/talks.tsv database/conferences.bib
+python3 libraries/tsv_to_bib_funding.py database/funding.tsv database/funding.bib
+python3 libraries/tsv_to_bib_awards.py database/awards.tsv database/awards.bib
+
+latexmk -synctex=1 -interaction=nonstopmode -file-line-error -lualatex CV.tex
