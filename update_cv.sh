f60e9f64a5e4d7c67721db92a214612128cc03db
Carlos Payá <carlos.paya@csic.es>
2025-07-08 10:59:11 +0200

    do not make pdf

--- update_cv.sh
+++ update_cv.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-if [ "$#" -ne 7 ]; then
-    echo "Usage: $0 <zotero_url> <talks_url> <outreach_url> <funding_url> <awards_url> <author_name> <output_pdf>"
+if [ "$#" -ne 6 ]; then
+    echo "Usage: $0 <zotero_url> <talks_url> <outreach_url> <funding_url> <awards_url> <author_name>"
     exit 1
 fi
 
@@ -13,7 +13,6 @@ OUTREACH_URL="$3"
 FUNDING_URL="$4"
 AWARDS_URL="$5"
 AUTHOR_NAME="$6"
-OUTPUT_PDF="$7"
 
 python3 libraries/fetch_publications.py "$ZOTERO_URL" "$AUTHOR_NAME"
 python3 libraries/js_to_bib.py conferences "$TALKS_URL" database/conferences.bib
@@ -21,6 +20,6 @@ python3 libraries/js_to_bib.py outreach "$OUTREACH_URL" database/outreach.bib
 python3 libraries/js_to_bib.py funding "$FUNDING_URL" database/funding.bib
 python3 libraries/js_to_bib.py awards "$AWARDS_URL" database/awards.bib
 
-latexmk -synctex=1 -interaction=nonstopmode -file-line-error -lualatex CV.tex
+#latexmk -synctex=1 -interaction=nonstopmode -file-line-error -lualatex CV.tex
 
-mv CV.pdf "$OUTPUT_PDF"
+#mv CV.pdf "$OUTPUT_PDF"
